8d3ab96947c7e252160b976d65b536ec1c8f12c8
Ainur <ainur9173944784@gmail.com>
2025-12-27 18:27:31 +0300

    Major update: added Kruta.GUI2, Kruta.Protocol and fixed table rotation logic

---
+++ Kruta.Shared/Models/Player.cs
@@ -0,0 +1,35 @@
+﻿using Kruta.Shared.Models.Cards;
+using Kruta.Shared.Models.Cards.Familiar;
+using Kruta.Shared.Models.Interfaces;
+using Kruta.Shared.Models.Tokens.JKSToken;
+using Kruta.Shared.Services.Static;
+using System.Collections.Generic;
+
+namespace Kruta.Shared.Models
+{
+    // Заглушка, отражающая общедоступное состояние одного игрока
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CurrentHealth { get; set; }
+        public int PowerGainedThisTurn { get; set; } // Мощь за текущий ход
+
+        public WizardPropertyToken WizardPropertyToken { get; set; } //ЖКС
+        public FamiliarCard Familiar { get; set; } //Фамильяр
+
+        // Карты, которые видят все (тут же будут ЖКС и Фамильяр)
+        public List<IOpenedCard> PublicCards { get; set; } = new();
+
+        // Закрытое состояние (нужно для сервера/логики, но не для GameStateMessage)
+        public List<ICard> Hand { get; set; } = new ();
+        public List<ICard> PlayerDeck { get; set; } = new ();
+        public List<ICard> Discard { get; set; } = new ();
+        public List<ICard> PlayedCardsThisTurn { get; set; } = new();
+
+        public void ShuffleDeck()
+        {
+            PlayerDeck = PlayerDeck.ShuffleCustom().ToList();
+        }
+    }
+}
